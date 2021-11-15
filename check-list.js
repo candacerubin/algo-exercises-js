145d2516877e4038a74d1c0f533d84e8f892202a
candacerubin <candacerubin0@gmail.com>
2021-11-14 17:56:11 -0800

    returns array of strings formatted by status

---
+++ check-list.js
@@ -0,0 +1,24 @@
+// Implement the function formatTaskList.
+// It should transform each task object into a string by placing
+// a green checkmark (u2705) in front of the title if the task is DONE.
+// Otherwise, use a red cross (u274c).
+
+function formatTaskList(tasks) {
+	let result = [];
+	for (const task of tasks) {
+		task.status === 'DONE'
+			? result.push(`\u2705 ${task.title}`)
+			: result.push(`\u274c ${task.title}`);
+	}
+	return result;
+}
+
+const tasks = [
+	{ id: 1, title: 'Start learning JS on CoderslangJS', status: 'DONE' },
+	{ id: 2, title: 'Write my first program', status: 'DONE' },
+	{ id: 3, title: 'Set clear and achievable goals', status: 'DONE' },
+	{ id: 4, title: 'Be patient and learn every day', status: 'IN_PROGRESS' },
+	{ id: 5, title: 'Join Coderslang community', status: 'IN_PROGRESS' },
+];
+
+console.log(formatTaskList(tasks));
