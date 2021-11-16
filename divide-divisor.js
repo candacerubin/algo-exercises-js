4678292a60d6eda912bf10c55de3e3bd6b999138
candacerubin <candacerubin0@gmail.com>
2021-11-15 20:36:29 -0800

    function returns function for division

---
+++ divide-divisor.js
@@ -0,0 +1,17 @@
+// Implement the function divideBy(divisor)
+// It should accept a number **divisor** and return a function that takes another number **n**
+// and divides it by the **divisor**.
+
+function divideBy(divisor) {
+	return (n) => {
+		return n / divisor;
+	};
+}
+
+const divideBy2 = divideBy(2);
+const divideBy5 = divideBy(5);
+
+console.log(divideBy2(10));
+console.log(divideBy2(20));
+console.log(divideBy5(10));
+console.log(divideBy5(20));
