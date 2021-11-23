94887c306c7b081915636c7ff897d830d20bf660
candacerubin <candacerubin0@gmail.com>
2021-11-23 00:27:37 -0800

    returns boolean indicating presence of anagram

--- anagrams.js
+++ anagrams.js
@@ -10,15 +10,51 @@ const anagrams = (s1, s2) => {
 		// if letter exists, increment, otherwise set to 1
 		lookup[letter] ? (lookup[letter] += 1) : (lookup[letter] = 1);
 	}
-	for (let i = 0; i < s2.length; i) {}
+	for (let i = 0; i < s2.length; i++) {
+		let letter = s2[i];
+		// if no letter exists, or letter === zero anagram is false
+		if (!lookup[letter]) {
+			return false;
+		} else {
+			lookup[letter] -= 1;
+		}
+	}
+	return true;
 };
 
-anagrams('restful', 'fluster'); // -> true
-anagrams('cats', 'tocs'); // -> false
-anagrams('monkeyswrite', 'newyorktimes'); // -> true
-anagrams('paper', 'reapa'); // -> false
-anagrams('elbow', 'below'); // -> true
-anagrams('tax', 'taxi'); // -> false
-anagrams('taxi', 'tax'); // -> false
-anagrams('night', 'thing'); // -> true
-anagrams('abbc', 'aabc'); // -> false
+// Solution using for/of loop
+// const anagrams = (s1, s2) => {
+//   const count = {};
+//   for (let char of s1) {
+//     if (!(char in count)) {
+//       count[char] = 0;
+//     }
+//     count[char] += 1;
+//   }
+
+//   for (let char of s2) {
+//     if (count[char] === undefined) {
+//       return false;
+//     } else {
+//       count[char] -= 1;
+//     }
+//   }
+
+//   for (let char in count) {
+//     if (count[char] !== 0) {
+//       return false;
+//     }
+//   }
+
+//   return true;
+// };
+
+console.log(anagrams('restful', 'fluster')); // -> true
+console.log(anagrams('cats', 'tocs')); // -> false
+console.log(anagrams('monkeyswrite', 'newyorktimes')); // -> true
+console.log(anagrams('paper', 'reapa')); // -> false
+console.log(anagrams('elbow', 'below')); // -> true
+console.log(anagrams('tax', 'taxi')); // -> false
+console.log(anagrams('taxi', 'tax')); // -> false
+console.log(anagrams('night', 'thing')); // -> true
+console.log(anagrams('abbc', 'aabc')); // -> false
