82ef5c28a5b6cb44dd00b2f7ca3ba93d26d829e4
candacerubin <candacerubin0@gmail.com>
2021-12-01 00:00:06 -0800

    returns most frequently occuring char in string

--- most-frequent.js
+++ most-frequent.js
@@ -1,6 +1,6 @@
 // Write a function, mostFrequentChar, that takes in a string as an argument. The function should return the most frequent character of the string. If there are ties, return the character that appears earlier in the string.
 
-// You can assume that the input string is non-empty.
+// You can assume that the input string is non-empty
 
 const mostFrequentChar = (s) => {
 	let count = {};
